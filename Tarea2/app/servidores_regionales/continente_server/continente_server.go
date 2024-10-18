a35d6a29c045c14018c911e8ae2d81ee18b09ead
tokifelipe <felipe.m.var@gmail.com>
2024-10-17 21:08:10 -0300

    Se  agrego la configuracion de despliege

--- Tarea2/app/servidores_regionales/continente_server/continente_server.go
+++ Tarea2/app/servidores_regionales/continente_server/continente_server.go
@@ -11,26 +11,26 @@ import (
 )
 
 type server struct {
-    pb.UnimplementedServidorRegionalServiceServer
+	pb.UnimplementedServidorRegionalServiceServer
 }
 
 // EnviarEstado implementa el método RPC para enviar el estado de un Digimon
 func (s *server) EnviarEstado(ctx context.Context, digimon *pb.EstadoDigimon) (*pb.Confirmacion, error) {
-    log.Printf("Recibido Digimon: Nombre: %s, Atributo: %s, Sacrificado: %v", digimon.Nombre, digimon.Atributo, digimon.Sacrificado)
-    return &pb.Confirmacion{Mensaje: "Estado recibido correctamente"}, nil
+	log.Printf("Recibido Digimon: Nombre: %s, Atributo: %s, Sacrificado: %v", digimon.Nombre, digimon.Atributo, digimon.Sacrificado)
+	return &pb.Confirmacion{Mensaje: "Estado recibido correctamente"}, nil
 }
 
 func main() {
-    lis, err := net.Listen("tcp", ":50053")
-    if err != nil {
-        log.Fatalf("Error al iniciar el listener: %v", err)
-    }
+	lis, err := net.Listen("tcp", ":50052")
+	if err != nil {
+		log.Fatalf("Error al iniciar el listener: %v", err)
+	}
 
-    grpcServer := grpc.NewServer()
-    pb.RegisterServidorRegionalServiceServer(grpcServer, &server{})
+	grpcServer := grpc.NewServer()
+	pb.RegisterServidorRegionalServiceServer(grpcServer, &server{})
 
-    log.Println("Continente Server corriendo en :50053")
-    if err := grpcServer.Serve(lis); err != nil {
-        log.Fatalf("Error al iniciar el servidor gRPC: %v", err)
-    }
+	log.Println("Continente Server corriendo en :50053")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Error al iniciar el servidor gRPC: %v", err)
+	}
 }
